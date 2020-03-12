25e740db187adc0b534f04c7a5cd2bfe2a53b1bc
rains <271102154@qq.com>
2020-03-12 17:40:33 +0800

    config配置

--- src/com/cn/wanxi/common/Config.java
+++ src/com/cn/wanxi/common/Config.java
@@ -1,12 +1,40 @@
 package com.cn.wanxi.common;
 
 import org.springframework.context.annotation.Configuration;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * 配置文件
  * 我们可以在这里做过滤器，做跨域，做编码
  */
 @Configuration
-public class Config {
+public class Config implements Filter {
 
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request=(HttpServletRequest)servletRequest;
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Max-Age", "600");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
